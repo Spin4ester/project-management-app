fac4c6c848d01243db0836ffa8b93e7b0dba7091
Spin4ester <bakibayev.r@gmail.com>
2022-11-14 01:28:24 +0100

    feat: add developer card component

---
+++ project-management-app/src/components/Developer-card/Developer-card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+type Props = {
+  name: string;
+};
+
+export const DeveloperCard = ({ name }: Props) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>Example Text</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+};
--- project-management-app/src/pages/Welcome/Welcome.tsx
+++ project-management-app/src/pages/Welcome/Welcome.tsx
@@ -1,5 +1,20 @@
+import { DeveloperCard } from 'components/Developer-card/Developer-card';
 import React from 'react';
+import YouTube from 'react-youtube';
+import './Welcome.css';
 
 export function Welcome() {
-  return <div className="welcome_container">Welcome</div>;
+  return (
+    <div className="welcome_container">
+      <div className="video_content">
+        <YouTube videoId="jfKfPfyJRdk" />
+      </div>
+      <div className="project_content"></div>
+      <div className="developers_content">
+        <DeveloperCard name="Rustam" />
+        <DeveloperCard name="Dasha" />
+        <DeveloperCard name="Anya" />
+      </div>
+    </div>
+  );
 }
