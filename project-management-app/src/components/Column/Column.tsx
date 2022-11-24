3689a5cd3c644f53267c09ebff74fa8852b1e863
Anna Makovskaya <hanna.makouskaya@newland.by>
2022-11-24 10:31:40 +0300

    feat: add addTask event

--- project-management-app/src/components/Column/Column.tsx
+++ project-management-app/src/components/Column/Column.tsx
@@ -1,40 +1,25 @@
 import { Task } from 'components/Task/Task';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Column.module.css';
 
 export const Column = (props) => {
+  const [tasks, setTasks] = useState(['task 1', 'task 2']);
+
+  const children = tasks.map((element, index) => {
+    return <Task title={element} key={index} />;
+  });
+
+  const addTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setTasks([...tasks, 'new task']);
+  };
+
   return (
     <div className={styles.container}>
       <h5>{props.name}</h5>
-      <div className={styles.content}>
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
+      <div className={styles.content}>{children}</div>
+      <div>
+        <button onClick={addTask}>Add task</button>
       </div>
     </div>
   );
--- project-management-app/src/components/Task/Task.tsx
+++ project-management-app/src/components/Task/Task.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './Task.module.css';
 import DeleteIcon from '../../assets/icons/delete.png';
 
-export const Task = () => {
+export const Task = (props) => {
   return (
     <div className={styles.container}>
-      <p>Task title</p>
+      <p>{props.title}</p>
       <img src={DeleteIcon} alt="Delete task" />
     </div>
   );
