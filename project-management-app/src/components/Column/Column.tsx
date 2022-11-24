15aaef99c24ff5e7a3c2fff4386ec4deee7585bb
Anna Makovskaya <hanna.makouskaya@newland.by>
2022-11-24 09:58:30 +0300

    feat: add addColumn event

--- project-management-app/src/components/Column/Column.tsx
+++ project-management-app/src/components/Column/Column.tsx
@@ -2,10 +2,10 @@ import { Task } from 'components/Task/Task';
 import React from 'react';
 import styles from './Column.module.css';
 
-export const Column = () => {
+export const Column = (props) => {
   return (
     <div className={styles.container}>
-      <h5>Column Name</h5>
+      <h5>{props.name}</h5>
       <div className={styles.content}>
         <Task />
         <Task />
--- project-management-app/src/pages/Board/Board.tsx
+++ project-management-app/src/pages/Board/Board.tsx
@@ -3,14 +3,27 @@ import { CreateColumn } from 'components/Modals/CreateColumn';
 import { CreateTask } from 'components/Modals/CreateTask';
 import { DeleteModal } from 'components/Modals/DeleteModal';
 import * as React from 'react';
+import { useState } from 'react';
 import styles from './Board.module.css';
 
 export const Board = () => {
+  const [columns, setColumns] = useState(['first', 'second']);
+
+  const children = columns.map((element, index) => {
+    return <Column name={element} key={index} />;
+  });
+
+  const addColumn = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setColumns([...columns, 'New']);
+  };
+
   return (
     <div className={styles.container}>
-      <Column />
-      <Column />
-      <Column />
+      <div className={styles.list}>{children}</div>
+      <div>
+        <button onClick={addColumn}>Add column</button>
+      </div>
       {/* <CreateColumn />
       <DeleteModal />
       <CreateTask /> */}
